baae7dc7b4bea2ebbdb56cf263806b130c1c3e1e
ankur-upflairs <ankur.upflairs@gmail.com>
2025-09-18 11:42:30 +0530

    code pushed

--- django/lmsApp/courses/models.py
+++ django/lmsApp/courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Courses(models.Model):
+    title=models.CharField()
+    
+    def __str__(self):
+        return f"<id ({self.id}) title={self.title}>"
+
+
+
